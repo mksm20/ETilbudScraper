b44508472836ef3dfaa5b6a998171ba8aecdf59b
mksm20 <mksm20@student.aau.dk>
2022-10-16 20:18:02 +0200

    fills db

--- Main.py
+++ Main.py
@@ -1,13 +1,29 @@
+import datetime
 import Scraper
 import SqlModule
 import obj
         
-    
+        
 ScraperInstance = Scraper.Main_Module
-list = ScraperInstance.run()
+for item in ScraperInstance.groceryList:
+    print(item.Name, item.Price, item.Store)
 Sql = SqlModule.Sql_Connect.run()
-SqlConnection = Sql[1]
-crsr = Sql[2]
-
-SqlDisconnect = SqlModule.Sql_Disconnect(SqlConnection)
-SqlDisconnect.close
+conn = Sql[1]
+crsr = Sql[0]
+table = """ CREATE TABLE IF NOT EXISTS Groceries(
+            Name TEXT,
+            Store TEXT,
+            Price REAL,
+            Date INTEGER 
+        ); """
+crsr.execute(table)
+conn.commit()
+time = datetime.datetime.now()
+time = str(time)
+for objects in ScraperInstance.groceryList:
+    sqlInsert = """INSERT INTO Groceries 
+    (Name, Store, Price, Date)
+    VALUES (?,?,?,?)"""
+    crsr.execute(sqlInsert, (str(objects.Name),str(objects.Store), str(objects.Price), time))
+    conn.commit()
+conn.close()
