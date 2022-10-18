5a3639c06b7ec3845ab27ebfe345ddf41e6fb4fc
mksm20 <mksm20@student.aau.dk>
2022-10-18 18:41:01 +0200

    added table to db, still needs better sorting

--- Main.py
+++ Main.py
@@ -2,31 +2,54 @@ import datetime
 import Scraper
 import SqlModule
 import obj
+import csv
         
+def isGrocery(grocery):
+    with open("groceryStoreList.csv", "r") as file:
+        data = csv.reader(file)
+        for field in data:
+            if grocery.Store == field:
+                return False
+        return True
+
         
 ScraperInstance = Scraper.Main_Module
-for item in ScraperInstance.groceryList:
+groceryList = ScraperInstance.run()
+for item in groceryList:
     print(item.Name, item.Price, item.Extra, item.Store)
 Sql = SqlModule.Sql_Connect.run()
 conn = Sql[1]
 crsr = Sql[0]
-table = """ CREATE TABLE IF NOT EXISTS Groceries(
+table = """ CREATE TABLE IF NOT EXISTS GroceriesRand(
             Name TEXT,
             Store TEXT,
             Extra TEXT,
             Price REAL,
             Date INTEGER 
         ); """
-crsr.execute(table)
-conn.commit()
+table2 = """ CREATE TABLE IF NOT EXISTS GroceriesEat(
+            Name TEXT,
+            Store TEXT,
+            Extra TEXT,
+            Price REAL,
+            Date INTEGER 
+        ); """
+tables = (table, table2)
+for table in tables:
+    crsr.execute(table)
+    conn.commit()
 time = str(datetime.datetime.now())
-for objects in ScraperInstance.groceryList:
-    sqlInsert = """INSERT INTO Groceries 
-    (Name, Store, Extra ,Price, Date)
-    VALUES (?,?,?,?,?)"""
+for object in groceryList:
+    inputStr = "GroceriesEat"
+    if not isGrocery(object):
+        inputStr = "GroceriesRand"
     extra = ""
-    if(objects.Extra is not None):
-        extra = str(objects.Extra)
-    crsr.execute(sqlInsert, (str(objects.Name),str(objects.Store),extra,str(objects.Price), time))
+    if(object.Extra is not None):
+        extra = str(object.Extra)
+    sqlInsert = f"""INSERT INTO {inputStr} 
+    (Name, Store, Extra ,Price, Date)
+    VALUES (?,?,?,?,?)"""    
+    crsr.execute(sqlInsert, (str(object.Name),str(object.Store),extra,str(object.Price), time))
     conn.commit()
 conn.close()
+
--- Scraper.py
+++ Scraper.py
@@ -8,45 +8,48 @@ import re
 
 
 class Main_Module:
-        groceryList = []
-        url = 'https://etilbudsavis.dk/tilbud'
-        path = '/Users/martin mortensen/Desktop/Div/Python/Projects/ScaperEtilbud/chromedriver'
-        driver = webdriver.Chrome(path)
-        driver.get(url)
-        for i in range(30):
-            driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-            time.sleep(1)
-        all_offers = driver.find_elements(By.TAG_NAME, 'li')
-        idx = 1  
-        for offers in all_offers:
-            try:                                   
-                name = offers.find_element(
-                    By.XPATH,
-                    f"""//*[@id="main"]/div[2]/ul
-                    /li[{idx}]/a/div/div[3]/header""").text
-                price = offers.find_element(
-                    By.XPATH, 
-                    f"""//*[@id="main"]/div[2]/ul
-                    /li[{idx}]/a/div/div[3]/div[2]/div/span""").text
-                store = offers.find_element(
-                    By.XPATH, 
-                    f"""//*[@id="main"]/div[2]/ul
-                    /li[{idx}]/a/div/div[3]/div[3]/div/div""").text
-                extra = offers.find_element(
-                    By.XPATH, 
-                    f"""/html/body/div[1]/div/div[2]/div[2]/div[2]/ul
-                    /li[{idx}]/a/div/div[3]/div[1]""").text
-                a = obj.grocery()
-                a.Name = name
-                a.Store = store
-                price = re.sub(" kr.", "", price)
-                a.Price = price
-                a.Extra = extra
-                idx = idx + 1
-                print(price, name)
-                groceryList.append(a)
-            except:
-                selenium.common.exceptions.NoSuchElementException 
-                continue
-        driver.quit()
+
+        def run():
+            groceryList = []
+            url = 'https://etilbudsavis.dk/tilbud'
+            path = '/Users/martin mortensen/Desktop/Div/Python/Projects/ScaperEtilbud/chromedriver'
+            driver = webdriver.Chrome(path)
+            driver.get(url)
+            for i in range(30):
+                driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+                time.sleep(1)
+            all_offers = driver.find_elements(By.TAG_NAME, 'li')
+            idx = 1  
+            for offers in all_offers:
+                try:                                   
+                    name = offers.find_element(
+                        By.XPATH,
+                        f"""//*[@id="main"]/div[2]/ul
+                        /li[{idx}]/a/div/div[3]/header""").text
+                    price = offers.find_element(
+                        By.XPATH, 
+                        f"""//*[@id="main"]/div[2]/ul
+                        /li[{idx}]/a/div/div[3]/div[2]/div/span""").text
+                    store = offers.find_element(
+                        By.XPATH, 
+                        f"""//*[@id="main"]/div[2]/ul
+                        /li[{idx}]/a/div/div[3]/div[3]/div/div""").text
+                    extra = offers.find_element(
+                        By.XPATH, 
+                        f"""/html/body/div[1]/div/div[2]/div[2]/div[2]/ul
+                        /li[{idx}]/a/div/div[3]/div[1]""").text
+                    a = obj.grocery()
+                    a.Name = name
+                    a.Store = store
+                    price = re.sub(" kr.", "", price)
+                    price = re.sub(",", ".", price)
+                    a.Price = price
+                    a.Extra = extra
+                    idx = idx + 1
+                    groceryList.append(a)
+                except:
+                    selenium.common.exceptions.NoSuchElementException 
+                    continue
+            driver.quit()
+            return groceryList
  
